a1ca4e75eb8411e101360ba1257c969de217f6fa
icang02 <ilmifaizan1112@gmail.com>
2025-01-07 21:43:00 +0800

    feat(favicon): add new favicon app

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -16,6 +16,7 @@ import {
   SidebarMenuSubItem,
   SidebarRail,
 } from "@/components/ui/sidebar";
+import Image from "next/image";
 
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
   const pathname = usePathname();
@@ -59,9 +60,14 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
           <SidebarMenuItem>
             <SidebarMenuButton size="lg" asChild>
               <Link href="#">
-                <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
-                  <GalleryVerticalEnd className="size-4" />
-                </div>
+                <Image
+                  src={"/logo.jpeg"}
+                  width={30}
+                  height={30}
+                  alt="logo"
+                  className="rounded"
+                  unoptimized
+                />
                 <div className="flex flex-col gap-0.5 leading-none">
                   <span className="font-semibold">Quizy</span>
                   <span className="">Administrator</span>
--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -10,7 +10,7 @@ const buttonVariants = cva(
     variants: {
       variant: {
         default:
-          "bg-primary text-primary-foreground shadow hover:bg-primary/90",
+          "bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 text-primary-foreground shadow hover:from-blue-500/90 hover:via-blue-600/90 hover:to-blue-700/90",
         destructive:
           "bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90",
         success:
